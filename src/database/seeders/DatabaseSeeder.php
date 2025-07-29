0913ca71b95f8c155bea27ba61c7705f361fad77
nozomi1129 <imo.4869.on@gmail.com>
2025-07-30 08:19:04 +0900

    database

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CategoriesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
         $this->call(ContactTableSeeder::class);
 
     }
